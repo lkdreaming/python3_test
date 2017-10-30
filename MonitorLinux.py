3e6affe01731c4a2e2dd47fd0dd19e8dd02a3260
Liuke <liuke19830822@qq.com>
2017-10-30 23:09:06 +0800

    增加MonitorLinux的打印調試

--- MonitorLinux.py
+++ MonitorLinux.py
@@ -5,27 +5,32 @@ class MonitorLinux(object):
     def __init__(self,pro):
         self.pro = pro
     def monitor(self):
-        res=os.popen('ps -ef | grep '+ self.pro +' | grep -v grep | wc -l','r').readlines()
-        res=''.join(res).strip()
+        res = os.popen('ps -ef | grep '+ self.pro +' | grep -v grep | wc -l','r').readlines()
+        res = ''.join(res).strip()
+        print(res)
         logging.basicConfig(filename='/var/log/' + self.pro + '.log', filemode="w",
                             format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
         if int(res) <= 0:
             logging.debug(self.pro+"has gone,now restarting it!")
-            #print(self.pro+"has gone,now restarting it!")
+            print(self.pro+"has gone,now restarting it!")
             try:
                 os.system('/etc/init.d/'+ self.pro + 'start')
-                logging.debug(self.pro + "is started")
+                logging.debug(self.pro + " is started")
+                print(self.pro + " is started")
             except OSError:
-                logging.debug(self.pro + "startup failed")
+                logging.debug(self.pro + " startup failed")
+                print(self.pro + " startup failed")
             else:
                 res = os.popen('ps -ef | grep ' + self.pro + ' | grep -v grep | wc -l', 'r').readlines()
                 res = ''.join(res).strip()
                 if int(res) <= 0:
-                    logging.debug(self.pro + "startup failed")
+                    logging.debug(self.pro + " startup failed")
+                    print(self.pro + " startup failed")
                 else:
-                    logging.debug(self.pro + "startup successfully")
+                    logging.debug(self.pro + " startup successfully")
+                    print(self.pro + " startup successfully")
         else:
-            logging.debug(self.pro + "is running!")
+            logging.debug(self.pro + " is running!")
 
 
 
